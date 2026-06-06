99cec62578eb4ce3c0050357d59610d66c0678dc
dnyaneshwar718 <dnyaneshwargurme53@gmail.com>
2026-06-06 10:58:07 +0530

    Delete code8.py

--- code8.py
+++
@@ -1,9 +0,0 @@
-#function for identifying even number or odd number
-num=int(input("Enter your num :"))
-def identifier(num):
-    if num%2==0:
-        print("even number")
-    else:
-        print("Odd number")
-
-identifier(num)
