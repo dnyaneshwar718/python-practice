c212169052a53ef0d6734c911c09553cf2b4d1ac
dnyaneshwar718 <dnyaneshwargurme53@gmail.com>
2026-06-10 09:08:35 +0530

    Update file_handling_2.py

--- all codes/file_handling_2.py
+++ all codes/file_handling_2.py
@@ -1,3 +1,4 @@
+#reding the lines in the file
 f=open("muheheh.txt","r")
 line2=f.readline()
 print(line2)
@@ -5,3 +6,6 @@ print(type(line2))
 line1=f.readline()
 print(line1)
 print(type(line1))
+#out put
+#sjfnkdbnfkdnf(output of line2)
+#a+b=14(output of line 1)
